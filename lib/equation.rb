b56ee22917e0217cbd1b487198eca5fbf8ad7522
akicho8 <akicho8@gmail.com>
2014-05-08 15:41:59 +0900

    Equation.create メソッド追加

--- lib/equation.rb
+++ lib/equation.rb
@@ -5,6 +5,11 @@ require "equation/bezier_curve"
 require "equation/linear_curve"
 require "equation/parabola_curve"
 
+require "active_support/core_ext/string/inflections"
+
 module Equation
-  # Your code goes here...
+  # Equation.create(:type => :linear, :x_range => 1..100, :y_range => 0..10000)
+  def self.create(type:, **params)
+    "equation/#{type}_curve".classify.constantize.friendly_create(params)
+  end
 end
