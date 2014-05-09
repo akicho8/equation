055bdb978b643fe93e6d7f0218480c8a1e55a4f9
akicho8 <akicho8@gmail.com>
2014-05-09 14:03:32 +0900

    組み合わせる例を追加

---
+++ examples/0150_example.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# 組み合わせる例
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'equation'
+require 'gnuplot'
+
+x_range = 1..100
+patitions = []
+patitions << {x_range: 1..30,   y_range: 0..1500,     type: :bezier, pull: 0.2}
+patitions << {x_range: 30..90,  y_range: 1500..4000,  type: :bezier, pull: -0.31}
+patitions << {x_range: 90..100, y_range: 4000..10000, type: :bezier, pull: -0.21}
+
+list = patitions.collect do |e|
+  {
+    :name => "#{e[:x_range]} => #{e[:pull]}",
+    :records => x_range.collect{|x|
+      if e[:x_range].include?(x)
+        {:x => x, :y => Equation.create(e).y_by_x(x)}
+      end
+    }.compact
+  }
+end
+Equation::Base.output_file(list, :title => "Equation::BezierCurve", :filename => "mix_curve.png")
