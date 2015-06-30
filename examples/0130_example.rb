14c3d84081f91ad0f913b4e3fac054148dbce019
akicho8 <akicho8@gmail.com>
2015-06-30 15:08:06 +0900

    Support ruby syntax

--- examples/0130_example.rb
+++ examples/0130_example.rb
@@ -2,6 +2,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'equation'
 require 'equation/level_support'
-Equation::Base.send(:include, Equation::LevelSupport)
+Equation::Base.include(Equation::LevelSupport)
 
 Equation::LinearCurve._create(15..15, 100..200).exp_by_lv(15) rescue $! # => #<ZeroDivisionError: divided by 0>
